6c460870b93c070a8dfb4a9c1c60c731382076b9
Mishu <124211865+mishhuu@users.noreply.github.com>
2023-03-26 23:14:23 +0200

    Update sitemap.xml.ts

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,30 +1,18 @@
-import {serverQueryContent} from "#content/server";
-import {SitemapStream, streamToPromise} from "sitemap";
+import { serverQueryContent } from '#content/server';
+import { SitemapStream, streamToPromise } from 'sitemap';
 
 export default defineEventHandler(async (event) => {
-
-    const visible = await serverQueryContent(event).where({visibility: 1}).only("_path").find();
-    const limited = await serverQueryContent(event).where({visibility: 2}).only("_path").find();
-    const posts = visible.concat(limited);
-
-    posts.push(
-        {
-            _path: "/"
-        }
-    );
-
-    const sitemap = new SitemapStream({
-        hostname: `https://${process.env.domain}`
-    });
-
-    for(const post of posts) {
-        sitemap.write({
-            url: post._path,
-            changefreq: "weekly"
-        });
-    }
-
-    sitemap.end();
-    return streamToPromise(sitemap);
-
+	// Fetch all documents
+	const docs = await serverQueryContent(event).find();
+	const sitemap = new SitemapStream({
+		hostname: 'https://blog.skyra.pw'
+	});
+	for (const doc of docs) {
+		sitemap.write({
+			url: doc._path,
+			changefreq: 'monthly'
+		});
+	}
+	sitemap.end();
+	return streamToPromise(sitemap);
 });
